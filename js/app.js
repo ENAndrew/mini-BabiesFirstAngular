4779ac931d14da15009589b004a8636a2c07f931
Erica Andrew <erica.nicole.andrew@gmail.com>
2016-02-14 20:19:05 -0700

    Added css, Angular CDN, app.js

---
+++ js/app.js
@@ -0,0 +1,7 @@
+(function(){
+
+var app = angular.module('friendsList', []);  //the empty array creates a new module, other items using the module will not be passed an array as angular.module('friendsList')
+
+
+})();
+
